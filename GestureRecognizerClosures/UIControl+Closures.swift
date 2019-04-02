d5914d3d3213f50d087e251d23cf0df821943360
vkcldhkd <vkcldhkd@gmail.com>
2019-04-02 10:29:01 +0900

    update to swift5.0

--- GestureRecognizerClosures/UIControl+Closures.swift
+++ GestureRecognizerClosures/UIControl+Closures.swift
@@ -3,7 +3,7 @@ import UIKit
 public extension UIControl {
 
     /// Adds a handler that will be invoked for the specified control events
-    public func on(_ controlEvents: UIControl.Event, invokeHandler handler: @escaping (UIControl) -> Void) -> AnyObject {
+    func on(_ controlEvents: UIControl.Event, invokeHandler handler: @escaping (UIControl) -> Void) -> AnyObject {
         let closureHandler = ClosureHandler(handler: handler, control: self)
         addTarget(closureHandler, action: ClosureHandlerSelector, for: controlEvents)
         var handlers = self.handlers ?? Set<ClosureHandler<UIControl>>()
@@ -13,7 +13,7 @@ public extension UIControl {
     }
 
     /// Removes a handler from the control
-    public func removeHandler(_ handler: AnyObject) {
+    func removeHandler(_ handler: AnyObject) {
         guard let handler = handler as? ClosureHandler<UIControl> else { return }
         removeTarget(handler, action: ClosureHandlerSelector, for: .allEvents)
         if var handlers = self.handlers {
